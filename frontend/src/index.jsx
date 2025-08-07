23e28e557e88d933d6f07f01a7417f8ba025e610
esNIMA <s.nima.mahmoodian@gmail.com>
2025-08-07 11:50:45 +0330

    import from root fixed

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import '@telegram-apps/telegram-ui/dist/styles.css';
 import './index.css';
-import Root from "@/components/App.jsx";
+import Root from "@/components/Root.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(<Root/>);
